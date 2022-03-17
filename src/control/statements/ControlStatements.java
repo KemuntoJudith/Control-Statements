342a165467f5a85766aecaaa8fb32059bf175531
JUDITH <JUDITH@DESKTOP-9CCJU8N>
2022-03-17 14:19:08 +0300

    StudentMarks

---
+++ src/control/statements/ControlStatements.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control.statements;   // while statement
+
+/**
+ *
+ * @author JUDITH
+ */
+public class ControlStatements {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int i=10;  
+    while(i<=30){  
+        System.out.println(i);  
+    i++;  
+     }  
+  }  
+} 
+
---
+++ src/control/statements/dowhilestatement.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control.statements;
+
+/**
+ *
+ * @author JUDITH
+ */
+public class dowhilestatement {
+    public static void main(String[] args) {
+    int i = 5, n = 20;
+    
+    do {
+      System.out.println(i);
+      i++;
+    } while(i <= n);
+  }
+    
+}
